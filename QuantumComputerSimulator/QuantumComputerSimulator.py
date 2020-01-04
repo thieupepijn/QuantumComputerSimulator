51f094888fc379ad8034a48f6b23349818e9a3b6
thieupepijn <thieupepijn@hotmail.com>
2020-01-05 00:22:24 +0100

    first commit

---
+++ QuantumComputerSimulator/QuantumComputerSimulator.py
@@ -0,0 +1,41 @@
+from random import seed
+from random import randint
+
+class QBit:
+    zeroprobability= 50
+    oneprobability = 50
+    
+    def __init__(self, initvalue):
+        if (initvalue == 0):
+            self.zeroprobability = 100
+            self.oneprobability = 0
+        elif (initvalue == 1):
+            self.zeroprobability = 0
+            self.oneprobability = 100
+
+    def read(self):
+       if (self.zeroprobability == 100):
+            return 0
+       elif(self.oneprobability == 100):
+            return 1
+       else:
+            seed()
+            random = randint(0, 1)
+            if (random == 0):
+                self.zeroprobability = 100
+                self.oneprobability = 0
+            elif (random == 1):
+                self.zeroprobability = 0
+                self.oneprobability = 100
+
+    def had(self):
+        self.zeroprobability = 50
+        self.oneprobability = 50
+
+
+
+   
+bit = QBit(1)
+bit.had()
+for _ in range(25):
+    print(bit.read())
