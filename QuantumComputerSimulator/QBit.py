5a3ee0687f230d14c728ad95e3d8216924db7764
thieupepijn <thieupepijn@hotmail.com>
2020-01-05 20:33:11 +0100

    prefixed zeroprabability and oneprobability with underscore to make them private

--- QuantumComputerSimulator/QBit.py
+++ QuantumComputerSimulator/QBit.py
@@ -2,32 +2,32 @@ from random import seed
 from random import randint
 
 class QBit:
-    zeroprobability= 50
-    oneprobability = 50
+    _zeroprobability= 50
+    _oneprobability = 50
     
     def __init__(self, initvalue):
         if (initvalue == 0):
-            self.zeroprobability = 100
-            self.oneprobability = 0
+            self._zeroprobability = 100
+            self._oneprobability = 0
         elif (initvalue == 1):
-            self.zeroprobability = 0
-            self.oneprobability = 100
+            self._zeroprobability = 0
+            self._oneprobability = 100
 
     def read(self):
-       if (self.zeroprobability == 100):
+       if (self._zeroprobability == 100):
             return 0
-       elif(self.oneprobability == 100):
+       elif(self._oneprobability == 100):
             return 1
        else:
             seed()
             random = randint(0, 1)
             if (random == 0):
-                self.zeroprobability = 100
-                self.oneprobability = 0
+                self._zeroprobability = 100
+                self._oneprobability = 0
             elif (random == 1):
-                self.zeroprobability = 0
-                self.oneprobability = 100
+                self._zeroprobability = 0
+                self._oneprobability = 100
 
     def had(self):
-        self.zeroprobability = 50
-        self.oneprobability = 50
+        self._zeroprobability = 50
+        self._oneprobability = 50
